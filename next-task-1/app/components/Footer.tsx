d95441b380b7b541224045c87e6caa367c131c44
mehedipolash <mehedipolash228@gmail.com>
2025-09-10 21:10:59 +0600

    updated lab task added

--- next-task-1/app/components/Footer.tsx
+++ next-task-1/app/components/Footer.tsx
@@ -1,7 +1,7 @@
 export default function Footer() {
   return (
     <footer className="fixed bottom-0 left-0 right-0 bg-indigo-700 text-white p-4 text-center">
-      <p>© 2025 EventHub. All rights reserved.</p>
+      <p>© 2025 EventHub. All rights reserved by EventHub.</p>
     </footer>
   );
 }
